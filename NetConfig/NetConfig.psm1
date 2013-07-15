0ef43060fea9e3f2672ea7bb61a426687966cd8b
Jason Ruiz <xorphenochx@gmail.com>
2013-07-15 10:45:50 -0400

    Initial code for Get-TargetResource

--- NetConfig/NetConfig.psm1
+++ NetConfig/NetConfig.psm1
@@ -1,3 +1,42 @@
 Function Get-TargetResource {
+    param
+    (
+    [ValidateSet("Present", "Absent")]
+    [string]$Ensure = "Present",
+
+    [ValidateNotNullOrEmpty()]
+    [string]$Adapter,
+
+    [ValidateSet("Manual", "DHCP")]
+    [string]$Type = "DHCP",
+
+    [array]$IPAddress,
+
+    [array]$DNSServers,
+
+    [string]$Gateway,
+
+    [boolean]$RegisterDNS
+    )
+
+    $getTargetResourceResult = $null;
     
+    $NetworkInformation = @(
+        Get-NetIPConfiguration -InterfaceAlias $Adapter
+        Get-DNSClient -InterfaceAlias $Adapter
+        Get-NetIPAddress -InterfaceAlias $Adapter -AddressFamily IPv4
+        )
+    
+    $getTargetResourceResult = @{
+        Ensure = $Ensure
+        Adapter = $Adapter
+        Type = ($NetworkInformation.SuffixOrigin | Where-Object {$_ -ne $null})
+        IPAddress = ($NetworkInformation.IPAddress | Where-Object {$_ -ne $null})
+        DNSServers = ($NetworkInformation.DNSServer.ServerAddresses | Where-Object {$_ -ne $null})
+        Gateway = $NetworkInformation.IPv4DefaultGateway.NextHop
+        RegisterDNS = ($NetworkInformation.RegisterThisConnectionsAddress | Where-Object {$_ -ne $null})
+        }
+
+    $getTargetResourceResult;
+
 }
